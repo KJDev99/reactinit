c6f429f3910e31ee3beb4c866bb8f0fb72ab528a
Jamshid <jamshidqayimov0399@gmail.com>
2026-06-22 12:17:01 +0500

    react init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/footer";
+import Hero from "./components/hero";
+import Navbar from "./components/navbar";
+import Product from "./components/product";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Product />
+      <Footer />
+    </div>
+  )
+}
